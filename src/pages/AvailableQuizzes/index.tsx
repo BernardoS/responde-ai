c603518da36175cdabf34dcab0bc5cf2e78c0d06
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-04-18 17:42:06 -0300

    Criação da página

---
+++ src/pages/AvailableQuizzes/index.tsx
@@ -0,0 +1,10 @@
+function AvailableQuizzes() {
+  return (
+    <div>
+      <h1>Available Quizzes</h1>
+      <p>List of available quizzes will be displayed here.</p>
+    </div>
+  );
+}
+
+export default AvailableQuizzes;
