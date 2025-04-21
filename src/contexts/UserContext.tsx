ed053721705ac94436374be4bb12dbc9c8389a93
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-04-21 18:15:17 -0300

    Ajuste do UserContext

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 import { getUserByEmail } from '../services/api';
 
 type Usuario = {
